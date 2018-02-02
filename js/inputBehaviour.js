4f01ffd3805d8137cebc77fe2d3b2853f0fe06f8
Marienkafer <marienbsequeram@gmail.com>
2018-02-02 11:47:06 -0300

    adding radiobutton behaviour with vanilla javascript and improving css style

---
+++ js/inputBehaviour.js
@@ -0,0 +1,26 @@
+function updateValue(value) {
+	var slider = document.getElementById("clientInput");
+	var clientNumber = document.getElementById("number-client");
+	var clientPricePlan = document.getElementById("client-price");
+	clientNumber.innerHTML = slider.value;
+	slider.oninput = function() {
+		clientNumber.innerHTML = this.value;
+		clientPricePlan.innerHTML = "$"+clientNumber.innerHTML*120;
+	}
+}
+
+function switchvalue(){
+	document.getElementById("cmdSwitch300").addEventListener("click", function(e){
+		var list = document.querySelector("ul");
+		list.appendChild(list.firstElementChild);
+		document.getElementById("300").checked= true;
+		document.getElementById("29").checked= false;
+	});
+	document.getElementById("cmdSwitch29").addEventListener("click", function(e){
+		var list = document.querySelector("ul");
+		list.appendChild(list.firstElementChild);
+		document.getElementById("29").checked= true;
+		document.getElementById("300").checked= false;
+	});
+
+}
